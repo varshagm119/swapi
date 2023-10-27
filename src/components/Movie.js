343ee4895598d90647e44d73eabdfd40998dd617
varshagm119 <varshagm96@gmail.com>
2023-10-27 13:29:16 +0530

    task 11

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,31 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  const deleteMovieHandler = async (id) =>{
+    console.log(id)
+    try{
+      const response = await fetch(`https://movielist-1f660-default-rtdb.firebaseio.com/movieList.json/${id}`,
+      {
+        method: 'DELETE',
+        headers:{
+          'Content-Type': 'application/json',
+        }
+      }
+      )
+      if(!response.ok){
+        throw new Error("Failed to delete the movie")
+      }
+    }
+    catch (error){
+      console.error('Error:',error)
+    }
+  }
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button onClick={() => deleteMovieHandler(props.id)}>Delete</button>
     </li>
   );
 };
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -8,6 +8,7 @@ const MovieList = (props) => {
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
+          id={movie.id}
           key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
